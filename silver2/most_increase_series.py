b461c03882d01a6fca6f29089196b45568180e86
정원균 <wjddnjsrbs97@naver.com>
2022-08-02 00:35:56 +0900

    coding exercise

--- silver2/most_increase_series.py
+++ silver2/most_increase_series.py
@@ -1,20 +1,14 @@
-
 num_series = int(input())
-longest = [] 
-dp = [0]*num_series
+dp = [0 for i in range(num_series)]
 
 temp = input().split()
 series = list(map(int,temp))
 
-
 for i in range(num_series):     
-    cur_max = series[i]
-    cnt = 1     
-    for j in range(i+1,num_series):
-        if series[j] > cur_max:
-            cur_max = series[j]
-            cnt+=1 
-    dp[i] = cnt     
+    for j in range(i):
+        if (dp[j] > dp[i]) and (series[i] > series[j]):
+            dp[i] = dp[j]
+    dp[i]+=1      
 
 print(max(dp))
 
