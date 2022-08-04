fa3e9e73c41ee4c07d523a96e8da7e22f9cdaf60
정원균 <wjddnjsrbs97@naver.com>
2022-08-04 23:35:58 +0900

    coding exercise

--- silver3/2n_tiling.py
+++ silver3/2n_tiling.py
@@ -1,15 +1,12 @@
-#Code by 정원균 
-#BaekJoon #11726
+# Code by 정원균
+# BaekJoon #11726
 
 target = int(input())
-DP = [0]*1001 
+DP = [0]*1001
 DP[1] = 1
 DP[2] = 2
 
-
-for i in range(3,1001):
+for i in range(3, 1001):
     DP[i] = DP[i-1]+DP[i-2]
 
-
-print(DP[target]%10007)
-
+print(DP[target] % 10007)
