70eb2c31a093435afd24981dbca893661e1e314e
정원균 <wjddnjsrbs97@naver.com>
2022-08-08 00:10:36 +0900

    coding exercise

--- silver2/linked_element.py
+++ silver2/linked_element.py
@@ -3,34 +3,31 @@
 import sys
 sys.setrecursionlimit(10000)
 
-def dfs(node):
 
+def dfs(node):
     visited[node] = 1
 
-    for i in graph[node]:
-        if visited[node] == 0:
-            dfs(i)
+    for e in graph[node]:
+        print(visited)
+        if visited[e] == 0:
+            dfs(e)
 
-    print(visited)
     return None
 
 
 node, edge = map(int, input().split())
-visited = [0]*10
-graph = [[] for _ in range(1001)]
+visited = [0]*(node+1)
+graph = [[] for _ in range(node+1)]
 connect = 0
 
 for i in range(edge):
-    
     node_1, node_2 = map(int, input().split())
     graph[node_1].append(node_2)
     graph[node_2].append(node_1)
 
-for i in range(1,node+1):
-    
-    if visited[i] == 0:
-        dfs(i)
-        connect+=1
-        
+for j in range(1, node+1):
+    if visited[j] == 0:
+        dfs(j)
+        connect += 1
 
 print(connect)
