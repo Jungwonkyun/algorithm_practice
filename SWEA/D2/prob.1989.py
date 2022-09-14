c601994079d814b1219feba4850867ebe828b8b5
정원균 <wjddnjsrbs97@naver.com>
2022-09-14 18:25:57 +0900

    coding exercise

---
+++ SWEA/D2/prob.1989.py
@@ -0,0 +1,16 @@
+
+test_case = int(input())
+check = 0
+
+for i in range(test_case):
+    input_string = input()
+    length = len(input_string)//2
+
+    for j in range(length):
+        if input_string[j] != input_string[-1-j]:
+            check = 0
+            break
+        else:
+            check = 1
+
+    print("#{} {}".format(i+1,check))     
