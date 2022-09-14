42e2bcc747452e4e9f81885f71f710e9969ec362
정원균 <wjddnjsrbs97@naver.com>
2022-09-14 18:37:57 +0900

    coding exercise

---
+++ SWEA/D2/prob.1986.py
@@ -0,0 +1,15 @@
+
+test_case = int(input())
+result = 0
+
+for i in range(test_case):
+    input_num = int(input())
+
+    #짝수 일 떄 
+    if input_num%2 == 0:
+        result = -input_num//2
+
+    else: 
+        result = -(input_num//2)+input_num    
+    
+    print("#{} {}".format(i+1,result))
