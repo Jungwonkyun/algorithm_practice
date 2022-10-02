d0c549a5e36ded5d8234244474ce28d553550a24
정원균 <wjddnjsrbs97@naver.com>
2022-10-02 20:41:47 +0900

    coding exercise

--- BJ_New/Brute_Force/bj_1065.py
+++ BJ_New/Brute_Force/bj_1065.py
@@ -4,14 +4,12 @@ cnt = 0
 for num in range(1,N+1):
 
     str_num = str(num)
-    
     if num < 100:
         cnt+=1
         continue
 
     else:
-        diff = int(str_num[0]) - int(str_num[1])
-        
+        diff = int(str_num[0]) - int(str_num[1]) 
         for i in range(1,len(str_num)):
             now_diff = int(str_num[i-1]) - int(str_num[i])
             if diff != now_diff:
---
+++ BJ_New/DFS/bj_2251.py
@@ -0,0 +1,16 @@
+#### 다시풀기 
+
+N,E,W,S,N = map(int,input().split())
+plane = [[0]*14 for _ in range(14)]
+
+def dfs(y,x):
+
+
+
+    return None 
+
+
+
+#이동할 횟수당 4가지의 경우의 수가 가능하다 총 4**N의 경우의 수가 나옴 
+for i in range(4**N):
+    pass
