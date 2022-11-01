fe47396eea497d827bbd38012b894456c130e643
정원균 <wjddnjsrbs97@naver.com>
2022-11-01 12:25:16 +0900

    coding exercise

---
+++ SWEA/D3/5215.py
@@ -0,0 +1,19 @@
+test_case = int(input())
+for n in range(1, test_case+1):
+    score = [0]
+    calorie = [0]
+    N, L = map(int, input().split())
+    DP = [[0]*10001 for _ in range(21)]
+    for _ in range(N):
+        t, k = map(int, input().split())
+        score.append(t)
+        calorie.append(k)
+
+    for i in range(1, N+1):
+        for j in range(1, L+1):
+            if j < calorie[i]:
+                DP[i][j] = DP[i-1][j]
+            else:
+                DP[i][j] = max(DP[i-1][j], DP[i-1][j-calorie[i]]+score[i])
+
+    print("#{} {}".format(n,DP[N][L]))
